a0045ce78b44a23084cf2007083bd604f3da3dbd
Tohura <shorabantohura@gmail.com>
2025-09-18 22:06:20 +0800

    modified:   app.py modified:   templates/login.html

--- app.py
+++ app.py
@@ -1,8 +1,26 @@
 from flask import Flask, render_template, request, redirect, url_for, session, flash
 import mysql.connector
+from flask_bcrypt import Bcrypt
+from flask_wtf import CSRFProtect
+from datetime import timedelta
+from flask_wtf.csrf import generate_csrf
+
 
 app = Flask(__name__)
-app.secret_key = "yoursecretkey"  # needed for session
+app.secret_key = "icsp"  # needed for session
+
+# Security Config
+app.config['SESSION_COOKIE_SECURE'] = True
+app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=10)
+
+# Extensions
+bcrypt = Bcrypt(app)
+csrf = CSRFProtect(app)
+csrf.init_app(app)
+
+@app.context_processor
+def inject_csrf_token():
+    return dict(csrf_token=generate_csrf)
 
 # Database connection
 def get_db_connection():
@@ -22,6 +40,10 @@ def login():
     name = request.form['name']
     password = request.form['password']
 
+    if not name or not password:
+        flash("Please fill in both fields")
+        return redirect(url_for('home'))
+
     conn = get_db_connection()
     cursor = conn.cursor(dictionary=True)
     cursor.execute("SELECT * FROM users WHERE name=%s AND password=%s", (name, password))
@@ -35,6 +57,7 @@ def login():
     else:
         flash("Invalid username or password")
         return redirect(url_for('home'))
+
 
 @app.route('/dashboard')
 def dashboard():
