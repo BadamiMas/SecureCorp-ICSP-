23c090960588089bbcee66b7537696492625cb2d
Tohura <shorabantohura@gmail.com>
2025-10-13 18:13:49 +0800

    modified:   static/css/test.css 	modified:   static/images/checked.png 	modified:   static/js/test.js 	modified:   templates/test.html

--- static/js/test.js
+++ static/js/test.js
@@ -14,25 +14,16 @@ closebtn.addEventListener("click", () => {
 })
 
 
+themeToggler.addEventListener("click", () => {
+    document.body.classList.toggle("dark-theme-variables");
+    themeToggler.querySelector("span:nth-child(1)").classList.toggle("active");
+    themeToggler.querySelector("span:nth-child(2)").classList.toggle("active");
+})
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+/* To-Do List */
 
 const inputBox = document.getElementById('input-box');
 const listContainer = document.getElementById('list');
