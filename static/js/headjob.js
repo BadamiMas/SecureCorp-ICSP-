69503fe54f1bd17ff542a0f21224b0fc86d83af5
Tohura <shorabantohura@gmail.com>
2025-10-16 22:29:16 +0800

    new file:   static/css/accjob.css 	new file:   static/css/headjob.css 	new file:   static/css/hrjob.css 	new file:   static/js/accjob.js 	new file:   static/js/headjob.js 	new file:   static/js/hrjob.js 	new file:   templates/acc_job.html 	new file:   templates/head_job.html 	new file:   templates/hr_job.html

---
+++ static/js/headjob.js
@@ -0,0 +1,84 @@
+const sidemenu = document.querySelector("aside");
+const menubtn = document.querySelector("#menu-bar");
+const closebtn = document.querySelector("#close-btn");
+
+
+const themeToggler = document.querySelector(".theme-toggler");
+
+menubtn.addEventListener("click", () => {
+    sidemenu.style.display = "block"
+})
+
+closebtn.addEventListener("click", () => {
+    sidemenu.style.display = "none"
+})
+
+
+themeToggler.addEventListener("click", () => {
+    document.body.classList.toggle("dark-theme-variables");
+    themeToggler.querySelector("span:nth-child(1)").classList.toggle("active");
+    themeToggler.querySelector("span:nth-child(2)").classList.toggle("active");
+})
+
+
+/* To-Do List */
+
+const inputBox = document.getElementById('input-box');
+const listContainer = document.getElementById('list');
+
+function addTask() {
+    if (inputBox.value === '') {
+        alert("You must write something!");
+    }
+    else {
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value = "";
+    saveData();
+}
+
+listContainer.addEventListener("click", function(e) {
+    if (e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if (e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+        saveData();
+    }
+}, false);
+
+function saveData() {
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+
+
+function showTask() {
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+
+showTask();
+
+
+
+
+
+
+// LOGOUT //
+
+let logoutTimer;
+function resetTimer() {
+  clearTimeout(logoutTimer);
+  logoutTimer = setTimeout(() => {
+    window.location.href = "/logout";
+  }, 600000); // 1 min inactivity
+}
+
+window.addEventListener("load", resetTimer);
+window.addEventListener("mousemove", resetTimer);
+window.addEventListener("keypress", resetTimer);
